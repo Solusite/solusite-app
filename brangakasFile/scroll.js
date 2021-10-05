7470f84a37a4a0abf96efe4565cf9720ca33da11
baharudin-solusite <baharudinfahrul85@gmail.com>
2021-10-05 18:30:44 +0700

    cc

---
+++ brangakasFile/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#news-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [1000, 2],
+    itemsMobile: [650, 1],
+    navigationText: false,
+    autoPlay: true,
+  });
+});
